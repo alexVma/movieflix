2c5228c8129e4b69f70e2d4c73d5108287a56356
Alex V <aminga@netlife.net.ec>
2023-08-21 19:42:15 -0500

    router

---
+++ src/app/movies/movies-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+
+const routes: Routes = [
+    {
+        path: '', component: LayoutComponent, children: [
+            { path: '', redirectTo: 'peliculas', pathMatch: 'full' },
+            { path: 'peliculas', component: PeliculaComponent }
+        ]
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class MoviesRoutingModule { 
+
+}
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PeliculaComponent } from './components/pelicula/pelicula.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { SharedModule  } from '../shared/shared.module';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
 @NgModule({
   declarations: [
     PeliculaComponent,
-    PeliculasComponent
+    PeliculasComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule,
