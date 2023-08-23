3def000daec8c6d8ab32689560e352eabef085ac
Alex V <aminga@netlife.net.ec>
2023-08-22 19:29:06 -0500

    fix 401 status

--- src/app/movies/services/peliculas.service.ts
+++ src/app/movies/services/peliculas.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
 
+  token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNmQxN2E1MjdkNmMzMThlM2VhZmNiOWMxYTE4MTNhYyIsInN1YiI6IjVjZmE4MzAyOTI1MTQxNjczN2MxZWM1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7fjGIjIuVoHVrG1by8RjJ2g2rAzIxXOCEzkKV9AHPqg';
+
+  httpOptions ={
+    headers: new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    })
+  };
+
   constructor(private http: HttpClient) {
-   }
+  }
 
   obtenerPeliculas() {
-    return this.http.get('https://api.themoviedb.org/3/trending/movie/day?language=en-US');
+    return this.http.get(
+      'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+      this.httpOptions);
   } 
 }
